4e80fba9a509162eb14427cfec1d6adc0eea9a63
gvemulapalli33 <gvemulapalli33@gmail.com>
2020-10-27 19:25:16 -0700

    add clear functionality

--- main.js
+++ main.js
@@ -10,23 +10,49 @@ const dishesMenu = {
 }
 
 function handleClick(event) {
-    if (!event.target.classList.contains('btn-cook')) {
-        return;
-    } 
-    const selections = document.querySelectorAll('input');
-    const dishType = [...selections].find((item) => item.checked).id;
-    generateDish(dishType);
+
+    if (event.target.name === 'dish') {
+        if(event.target.checked) {
+            $button.disabled = false;
+        }
+    }
+
+    if (event.target.classList.contains('btn-cook')) {
+        const selections = document.querySelectorAll('input');
+        const dishType = [...selections].find((item) => item.checked).id;
+        generateDish(dishType);
+    }
+
+    if (event.target.classList.contains('clear')) {
+        $message.classList.add('cook');
+        const message = document.querySelector('.message');
+        message.remove();
+    }
+}
+
+function getEntireMeal() {
+    let sidesIndex = Math.floor(Math.random() * dishesMenu['sides'].length);
+    let mainsIndex = Math.floor(Math.random() * dishesMenu['mains'].length);
+    let dessertsIndex = Math.floor(Math.random() * dishesMenu['desserts'].length);
+
+    return `${dishesMenu['mains'][mainsIndex]}! with a of ${dishesMenu['sides'][sidesIndex]} and ${dishesMenu['desserts'][dessertsIndex]} for dessert!`;
+}
+
+function getDish(dishType) {
+    let randomIndex = Math.floor(Math.random() * dishesMenu[dishType].length);
+    let dish = dishesMenu[dishType][randomIndex];
+    return dish;
 }
 
 function generateDish(dishType) {
-    let randomIndex = Math.floor(Math.random() * dishesMenu[dishType].length);
-    let dish = dishesMenu[dishType][randomIndex];
     $message.classList.remove('cook');
     $message.innerHTML = 
     `<section class="message">
         <span class="text"><em>You should make</em></span> 
-        <p class="dishName">${dish}!</p>
-    </section>`;
+        <p class="dishName">${dishType === 'entireMeal' ? 
+        getEntireMeal() : getDish(dishType)}</p>
+        <button class="clear">clear</button>
+     </section>`;
 }
 
 document.addEventListener('click', handleClick);
